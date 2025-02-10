f2300017d8968c0b108f1f4ae1f500c25c1cd2f3
JedizR <teenday.jedi@gmail.com>
2025-02-10 19:09:16 +0700

    foward done

--- gaussian_elim.py
+++ gaussian_elim.py
@@ -5,14 +5,40 @@ def generate_random_matrix_and_answer(n):
     answer = [random.randint(1, 100) for _ in range(n)]
     return matrix, answer
 
-n = 5
+def show_matrix(matrix, answer):
+    for i in range(n):
+        for j in range(n+1):
+            if j == n:
+                print(f" | {answer[i]}")
+            else:
+                print(matrix[i][j], end=" ")
+            
+        print()
+    
+def forward(matrix, answer):
+    for i in range(n):
+        if matrix[i][i] != 1:
+            factor = matrix[i][i]
+            for j in range(n):
+                matrix[i][j] /= factor
+            answer[i] /= factor
+        for j in range(i+1, n):
+            factor = -1.0 * matrix[j][i]
+            for k in range(n):
+                matrix[j][k] += factor * matrix[i][k]
+            answer[j] += factor * answer[i]
+                
+        print("\nModified Matrix:")
+        show_matrix(matrix, answer)
+            
+n = 3
 matrix, answer = generate_random_matrix_and_answer(n)
+# matrix = [[1, 1, 1], [3, 2, 1], [2, -1, 4]]
+# answer = [6, 10, 12]
 
-for i in range(n):
-    for j in range(n+1):
-        if j == n:
-            print(f" | {answer[i]}")
-        else:
-           print(matrix[i][j], end=" ")
-        
-    print()
+
+print("Original Matrix:")
+show_matrix(matrix, answer)
+forward(matrix, answer)
+print("\nModified Matrix:")
+show_matrix(matrix, answer)
